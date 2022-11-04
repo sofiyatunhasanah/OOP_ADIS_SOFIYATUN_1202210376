bed81bb064db4104111c75c9c9e9d5d53a50d0cb
sofiyatunhasanah <116246577+sofiyatunhasanah@users.noreply.github.com>
2022-11-04 08:02:56 +0700

    Add files via upload

---
+++ User.java
@@ -0,0 +1,14 @@
+public class User {
+    public static void main(String[] args){
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+        Scanner keyboard = new Scanner();
+        nama = keyboard.nextLine();
+        System.out.println(" Nama : " + nama);
+        hp = keyboard.nextInt();
+        System.out.println(" No. HP : " + hp);
+        System.out.println("Register Success");
+        System.out.println("=====================");
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
