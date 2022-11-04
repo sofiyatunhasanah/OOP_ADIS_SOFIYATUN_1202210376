64395b4b35061e3d300793aaa57e28d769798205
sofiyatunhasanah <116246577+sofiyatunhasanah@users.noreply.github.com>
2022-11-04 08:06:48 +0700

    Add files via upload

---
+++ Modul1/src/Database.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
+
+String Menu;
+String Pilih_Menu;
+String Keluar;
+
+public class Database {
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+    public static void main(String[] args){
+        for (int i = 0; i < 5; i++) (
+            new Menu();
+            System.out.println("1. Menu\n2. Pilih Menu\n3. Keluar ");
+        )
+    }
+    // TODO Create Method to Insert Menu to Database
+    public void setName(String name){
+        this.name = name;
+    public void setNumb (Integer numb){
+        this.numb = numb;
+    }
+    }
+        
+    }
+
+    // TODO Create Method to Show Menu from Database
+
+    // TODO Create Method to Search Menu from Database
+
+
+
