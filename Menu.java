0e2fbbe9869d2e2209f58f350196d8150b53627e
sofiyatunhasanah <116246577+sofiyatunhasanah@users.noreply.github.com>
2022-11-04 08:38:17 +0700

    Delete Menu.java

--- Menu.java
+++
@@ -1,9 +0,0 @@
-public class Menu {
-    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
-    String name;
-    String category;
-    Int numb;
-    public static void main(String[] args)
-    System.out.println("1. Menu\n2. Pilih Menu\3.Keluar");
-    Int -
-}
